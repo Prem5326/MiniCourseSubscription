2ade6af06386cd0a127f83711e4c4f09ac13d36f
prem naik <premchandhhan@gmil.com>
2026-02-03 19:52:27 +0530

    backend deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: "*", // later you can restrict to vercel domain
+}));
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
